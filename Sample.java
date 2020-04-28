8e46996d8cc612bc4edcae4ce774b90e13b5dbe9
rajasekhar5c1 <rajasekhar5c1@gmail.com>
2020-04-27 19:21:01 -0500

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,16 @@
+
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		 String rajStr = new String();
+		 rajStr = "Hello world";
+		 
+		 int length;
+		 length = rajStr.length();
+		 System.out.println(length);
+
+		 
+	}
+
+}
